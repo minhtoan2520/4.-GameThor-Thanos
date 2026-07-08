039b0cd3533410a9189f06d426586eb3dce314e0
minhtoan <minhtoan2520@gmail.com>
2026-07-08 10:07:19 +0700

    update

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ import src.Hero;
 public class App {
     public static void main(String[] args) {
         Hero thor = new Hero("Thor", 200);
-        Hero thanos = new Hero("Thanos", 150);
+        Hero thanos = new Hero("Thanos", 250);
 
         System.out.println(thor.showInfo());
         System.out.println(thanos.showInfo());
