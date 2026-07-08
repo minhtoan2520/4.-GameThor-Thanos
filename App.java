bedd6c8882f46a43ca7c4a949c9ede831a9052d9
minhtoan <minhtoan2520@gmail.com>
2026-07-08 10:04:32 +0700

    update

--- App.java
+++ App.java
@@ -9,23 +9,6 @@ public class App {
         System.out.println(thanos.showInfo());
         System.out.println("START!!!");
 
-        System.out.println("============================================");
-        int count = 1;
-        while (thor.hp > 0 && thanos.hp > 0) {
-
-            System.out.println("Hiep dau: " + count);
-            count++;
-
-            thor.attack(thanos);
-            System.out.println(thanos.getName() + thanos.getHp());
-
-            thanos.attack(thor);
-            System.out.println(thor.getName() + thor.getHp() + "\n--------------------------------------------");
-        }
-        if (thanos.hp > 0) {
-            System.out.println(thanos.getName() + " WIN");
-        } else {
-            System.out.println(thor.getName() + " WIN");
-        }
+        Hero.battle(thor, thanos);
     }
 }
--- src/Hero.java
+++ src/Hero.java
@@ -31,11 +31,34 @@ public class Hero {
             System.out.println(hero.name + " da chet");
         } else {
             hero.hp -= this.damage;
-            System.out.println(this.name + " tan cong " + hero.name + " voi sat thuong " + this.damage);
+            System.out.println(this.name + " tan cong " + hero.name + " voi sat thuong: " + this.damage);
         }
     }
 
     // public void attack() {
     // System.out.println("Không có nhân vật để tấn công");
     // }
+
+    public static void battle(Hero h1, Hero h2) {
+        System.out.println("============================================");
+        int count = 1;
+        while (h1.hp > 0 && h2.hp > 0) {
+
+            System.out.println("Hiep dau: " + count);
+            count++;
+
+            h1.attack(h2);
+            System.out.println(h2.getName() + h2.getHp());
+
+            h2.attack(h1);
+            System.out.println(h1.getName() + h1.getHp() + "\n--------------------------------------------");
+        }
+        if (h1.hp <= 0) {
+            System.out.println(h1.getName() + " da chet");
+            System.out.println(h2.getName() + " WIN");
+        } else if (h2.hp <= 0) {
+            System.out.println(h2.getName() + " da chet");
+            System.out.println(h1.getName() + " WIN");
+        }
+    }
 }
